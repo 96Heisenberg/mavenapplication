1d23863de812cd21f547ca837f76ba08a714b63d
96Heisenberg <swapnilsen96@gmail.com>
2020-12-19 14:19:56 +0530

    1st Commit

--- src/main/java/com/crackjob/mavenapplication/App.java
+++ src/main/java/com/crackjob/mavenapplication/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Crackjob!" );
+        System.out.println( "Hello Crackjob abcd efg" );
     }
 }
