92001b044f93cedb920bc8df39ab1f9b1f6b008f
SayakNandy20 <sayaknandy11@gmail.com>
2020-12-26 20:16:55 +0530

    updation of code

--- src/main/java/com/crackjob/mavenapplication/App.java
+++ src/main/java/com/crackjob/mavenapplication/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Crackjobs!" );
+        System.out.println( "Hello Students of Crackjobs!" );
     }
 }
