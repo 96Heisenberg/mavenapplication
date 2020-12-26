5ab053391d3b5cb11df24543a6ad641f63564d91
SayakNandy20 <sayaknandy11@gmail.com>
2020-12-26 20:25:55 +0530

    upadation of app.java(2)

--- src/main/java/com/crackjob/mavenapplication/App.java
+++ src/main/java/com/crackjob/mavenapplication/App.java
@@ -10,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Students of Crackjobs!" );
+        System.out.println("Learn coding and make innovative projects");
     }
 }
